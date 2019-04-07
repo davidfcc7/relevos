2cb603f29d15b63241994a7e3f0c9526c9adc596
David <David@David>
2019-04-06 22:34:15 -0500

    clase utilitario: Creacion Random

---
+++ Relevos/src/main/java/com/mycompany/relevos/Utilitario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.relevos;
+
+/**
+ *
+ * @author David
+ */
+public class Utilitario {
+    
+    public static int Random(){
+        int random = (int) ((Math.random()* 3) + 1);
+        return random;}
+}
